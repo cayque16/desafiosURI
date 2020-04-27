801e63006c2fe71e79b9a7f96210679924152eca
Cayque Santos <cayque_santos16@hotmail.com>
2020-04-27 08:58:19 -0300

    Correcao do problema 1035, solucao aceita.

--- python/1035.py
+++ python/1035.py
@@ -3,8 +3,8 @@ a,b,c,d = map(int,input().split(" "))
 teste1 = b > c
 teste2 = d > a
 teste3 = (c+d) > (a+b)
-teste4 = (c%2==0) and (d%2==0)
-teste5 = a%2==0
+teste4 = (c > 0) and (d > 0)
+teste5 = (a%2)==0
 
 if(teste1 and teste2 and teste3 and teste4 and teste5):
 	print("Valores aceitos")
