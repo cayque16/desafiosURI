38c19ea051a5b2a182a69dacf87311d4c0c92959
Cayque Santos <cayque_santos16@hotmail.com>
2020-05-09 13:18:49 -0300

    Removendo arquivos movidos para outra pasta.

--- 1120.py
+++
@@ -1,9 +0,0 @@
-while True:
-  tecla,texto = input().split(" ")
-  if tecla == '0' or texto == '0':
-    break  
-  texto = texto.replace(tecla,'')
-  if texto != '':
-    print(int(texto))
-  else:
-    print('0')
