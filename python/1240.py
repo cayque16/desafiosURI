d6198ee06704ab829cae25224bd6e87ad6228770
Cayque Santos <cayque_santos16@hotmail.com>
2020-05-09 14:00:57 -0300

    Enviando solucao do problema 1240 sem ser testada no site. Apenas para ter salva a versao com os comentarios de teste.

--- python/1240.py
+++ python/1240.py
@@ -1,8 +1,13 @@
 casos = int(input())
 for i in range(casos):
   texto1,texto2 = input().split(" ")
-  x = texto1.find(texto2)
-  if x >= 0:
+  pos_texto2_in_texto1 = len(texto1)-len(texto2)
+#   print(texto1[tam_texto2:])
+  if texto2 == texto1[pos_texto2_in_texto1:]:
     print('encaixa')
   else:
     print('nao encaixa')
+
+# 0123456    01234
+# 5678690->7 78690->5
+#   78690
