66dcf87db2bb46bdc5667755f334806ce27e6d37
Cayque Santos <cayque_santos16@hotmail.com>
2020-05-11 13:28:54 -0300

    Solucao do problema 1141 criada, mas esta dando Time limit exceeded.

---
+++ python/1141.py
@@ -0,0 +1,27 @@
+while True:
+    casos = int(input())
+    if casos == 0:
+        break
+    lista = []
+    for i in range(casos):
+        lista.append(input())
+    lista.sort(key=lambda item: len(item))
+
+    lista_final = []
+    lista_temp = []
+    for i in range(len(lista)):
+        lista_temp.append(lista[0])
+        for j in lista:
+            item_atual = lista_temp[-1]
+            if item_atual == j:
+                pass
+            elif item_atual in j:
+                lista_temp.append(j)
+        lista_final.append(lista_temp)
+        lista_temp = []
+
+    maior = 0
+    for i in lista_final:
+        if maior < len(i):
+            maior = len(i)
+    print(maior)
